f03e88d9eb5aa8b7604c6653efbd9fa638705997
Pedro Leandro Gomes da Silva <pedro.silva@ma.senac.br>
2026-05-06 17:28:35 -0300

    refactor(controller): adicionar permissões para o DashboardController

--- app/Controllers/Teacher/DashboardController.php
+++ app/Controllers/Teacher/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Teacher;
 
 use App\Core\Auth;
 use App\Core\Controller;
+use App\Core\Permission;
 use App\Models\Ticket;
 use App\Models\User;
 
@@ -13,7 +14,7 @@ class DashboardController extends Controller
     {
         parent::__construct("App");
 
-        Auth::requireRole(User::TEACHER);
+        Auth::requirePermission(Permission::VIEW_REQUESTER_DASHBOARD);
     }
 
     public function index(): void
