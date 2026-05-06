2ba3366b7bcfdf4bcf915b231aaf5e558f4dc2f7
Pedro Leandro Gomes da Silva <pedro.silva@ma.senac.br>
2026-05-06 17:11:26 -0300

    feat(core): adicionar uma nova classe para agrupar as permissões

---
+++ app/Core/Permission.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Core;
+
+final class Permission
+{
+    //Chamados
+    public const OPEN_TICKET = 'abrir_chamado';
+    public const VIEW_MY_TICKET = 'ver_meus_chamados';
+    public const VIEM_ALL_TICKET = 'ver_todos_chamados';
+    public const COMENT_TICKET = 'comentar_chamado';
+    public const ATTACH_FILE_TICKET = 'anexar_arquivo_chamado';
+    public const CONFIRM_RESOLUTION = 'confirmar_resolucao';
+    public const REOPEN_TICKET = 'reabrir_chamado';
+    public const ASSIGN_TICKET = 'atribuir_chamado';
+    public const TAKE_TICKET = 'assumir_chamado';
+    public const EDIT_TICKET = 'editar_chamado';
+    public const CLOSE_TICKET = 'fechar_chamado';
+    public const ARCHIVE_TICKET = 'arquivar_chamado';
+    public const DELETE_TICKET = 'excluir_chamado';
+    public const VIEW_TICKET_HISTORY = 'ver_historico_chamado';
+    public const VIEW_MY_ASSIGNED_TICKET = 'ver_chamados_atribuidos_a_mim';
+
+    //COMENTARIOS
+    public const EDIT_OWN_COMMENT = 'editar_proprio_comentario';
+    public const DELETE_COMMENT = 'excluir_proprio_comentario';
+    public const DELETE_ANY_COMMENT = 'excluir_qualquer_comentario';
+
+
+    //ANEXOS
+    public const DELETE_OWN_ATTACHMENT = 'excluir_proprio_anexo';
+    public const DELETE_ANY_ATTACHMENT = 'excluir_qualquer_anexo';
+    public const DOWNLOAD_ATTACHMENT = 'download_anexo';
+
+    //Dashboard
+    public const VIEW_REQUESTER_DASHBOARD = 'ver_dashboard_solicitante';
+    public const VIEW_TECHICIAN_DASHBOARD = 'ver_dashboard_tecnico';
+    public const VIEW_MANAGER_DASHBOARD = 'ver_dashboard_gestor';
+
+    //Relatório
+    public const VIEW_REPORTS = 'ver_relatorios';
+    public const EXPORT_REPORTS = 'exportar_relatorios';
+
+    //Usuário
+    public const VIEW_USERS = 'ver_usuarios';
+    public const CREATE_USER = 'criar_usuario';
+    public const EDIT_USER = 'editar_usuario';
+    public const TOGGLE_USER_STATUS = 'ativar_inativar_usuario';
+    public const DELETE_USER = 'excluir_usuario';
+    public const RESET_USER_PASSWORD = 'redefinir_senha_usuario';
+    public const VIEW_USER_LAST_ACCESS = 'ver_ultimo_acesso_usuario';
+
+    //Perfils
+    public const VIEW_ROLES = 'ver_perfis';
+    public const CREATE_ROLE = 'criar_perfil';
+    public const EDIT_ROLE = 'editar_perfil';
+    public const DELETE_ROLE = 'excluir_perfil';
+    public const MANAGE_ROLE_PERMISSIONS = 'ver_permissoes';
+
+    //Departamentos
+    public const VIEW_DEPARTMENTS = 'ver_departamentos';
+    public const CREATE_DEPARTMENT = 'criar_departamento';
+    public const EDIT_DEPARTMENT = 'editar_departamento';
+    public const DELETE_DEPARTMENT = 'excluir_departamento';
+    public const LINK_USER_DEPARTMENT = 'vincular_usuario_departamento';
+    public const UNLINK_USER_DEPARTMENT = 'desvincular_usuario_departamento';
+
+    //Categoris
+    public const VIEW_CATEGORIES = 'ver_categorias';
+    public const CREATE_CATEGORY = 'criar_categoria';
+    public const EDIT_CATEGORY = 'editar_categoria';
+    public const DELETE_CATEGORY = 'excluir_categoria';
+
+    //Escolas
+    public const VIEW_SCHOOLS = 'ver_escolas';
+    public const CREATE_SCHOLAR = 'criar_escola';
+    public const EDIT_SCHOLAR = 'editar_escola';
+    public const DELETE_SCHOLAR = 'excluir_escola';
+
+    //Perfil pessoal
+    public const EDIT_OWN_PROFILE = 'editar_proprio_perfil';
+    public const CHANGE_OWN_PASSWORD = 'alterar_propria_senha';
+
+    //Sistema
+    public const VIEW_SYSTEM_LOG = 'ver_log_sistema';
+    public const MANAGE_SESSIONS = 'gerenciar_sessoes';
+}
