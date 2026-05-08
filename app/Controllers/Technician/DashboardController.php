4f460faf5e37427d19752af8c91dfb3181a3d2f7
Pedro Leandro Gomes da Silva <pedro.silva@ma.senac.br>
2026-05-08 14:34:55 -0300

    refactor(controller): adicionar chamado do método requirePermission nos métodos construct e index de DashboardController do Technician

--- app/Controllers/Technician/DashboardController.php
+++ app/Controllers/Technician/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Technician;
 
 use App\Core\Auth;
 use App\Core\Controller;
+use App\Core\Permission;
 use App\Models\Ticket;
 use App\Models\User;
 
@@ -13,11 +14,13 @@ class DashboardController extends Controller
     {
         parent::__construct("App");
 
-        Auth::requireRole(User::TECHNICIAN);
+        Auth::requirePermission(Permission::VIEW_TECHNICIAN_DASHBOARD);
     }
 
     public function index(): void
     {
+        Auth::requirePermission(Permission::VIEW_TECHNICIAN_DASHBOARD);
+
         $ticketModel = new Ticket();
         $tickets = (new Ticket())->ticketsOrderedByStatusPriorityAndOpeningDate();
 
