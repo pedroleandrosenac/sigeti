4c34cf4829683e87bc386ac4ffaab244aff5629d
Ezequiel Keslley Viana Santos da Cruz <ezequielcx14@gmail.com>
2026-05-14 14:40:14 -0300

    feat(controller): adicionar método index ao Admin\\DepartmentController

---
+++ app/Controllers/Admin/DepartmentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Core\Auth;
+use App\Core\Controller;
+use App\Core\Message;
+use App\Core\Permission;
+use App\Models\Department\Department;
+
+class DepartmentController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct("App");
+
+        Auth::requirePermission(Permission::VIEW_DEPARTMENTS);
+    }
+
+    public function index(): void
+    {
+        Auth::requirePermission(Permission::VIEW_DEPARTMENTS);
+        
+        $departmentModel = new Department();
+        
+        $departments = $departmentModel
+            ->orderBy("name", "ASC")
+            ->orderBy("deleted_at", "DESC")
+            ->get();
+        
+        echo $this->view->render("admin/department/index", [
+            "departments" => $departments
+        ]);
+        
+        clear_old();
+    }
+
+}
