7308bdb4e25723924dbf399f44f99e9a3839c216
Náyla Gabrielle Soares de Sousa <gabriellesousasousa24@gmail.com>
2026-05-14 14:43:19 -0300

    feat(controller): adicionar metodo store ao Admin\\UserController

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Admin;
 
 use App\Core\Auth;
 use App\Core\Controller;
+use App\Core\Message;
 use App\Core\Permission;
 use App\Models\Role\Role;
 use App\Models\User;
@@ -45,5 +46,53 @@ class UserController extends Controller
         clear_old();
 
     }
+    public function store(?array $data): void
+    {
+        Auth::requirePermission(Permission::CREATE_USER);
 
+        $this->validateCsrfToken($data, "/admin/usuarios/cadastrar");
+
+        $newUser = new User();
+
+        try {
+
+            $newUser->fill([
+                "name" => $data["name"],
+                "email" => $data["email"],
+                "password" => $data["password"],
+                "role_id" => $data["role_id"],
+                "status" => $data["status"]
+            ]);
+
+            $errors = array_merge(
+                $newUser->validate($data),
+                $newUser->validateBusinessRule()
+            );
+
+
+            if ($errors) {
+
+                flash_old($data);
+
+                foreach ($errors as $error) {
+                    Message::warning($error);
+                }
+
+                redirect("/admin/usuarios/cadastrar");
+            }
+
+            $newUser->save();
+
+
+
+        } catch (\InvalidArgumentException $invalidArgumentException) {
+            Message::error($invalidArgumentException->getMessage());
+            redirect("/admin/usuarios/cadastrar");
+            return;
+        }
+
+        Message::success("Usuário cadastrado com sucesso!");
+        redirect("/admin/usuarios/editar/" . $newUser->getId());
+
+    }
     }
