b9d6b63444ef0e7a15c93b0b951f0f624fd0328b
Nayra Geovana de Sousa Rodrigues <nararodrygues530@gmail.com>
2026-05-14 16:01:15 -0300

    feat(controller): adicionar método index ao ProfileController

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -5,13 +5,30 @@ namespace App\Controllers;
 use App\Core\Auth;
 use App\Core\Controller;
 use App\Core\Permission;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
     public function __construct()
     {
         parent::__construct('App');
+
         Auth::requirePermission(Permission::EDIT_OWN_PROFILE);
+    }
+
+    public function index(): void
+    {
+        $user = User::find(Auth::user()->id);
+
+
+        $layout = $this->resolveLayout();
+
+        echo $this->view->render('account/profile', [
+            'user' => $user,
+            'layout' => $layout
+        ]);
+
+        clear_old();
     }
 
     private function resolveLayout(): string
@@ -23,7 +40,7 @@ class ProfileController extends Controller
         if (Auth::hasPermission(Permission::VIEW_TECHNICIAN_DASHBOARD)) {
             return 'technician/app';
         }
-        if (Auth::hasPermission(Permission::VIEW_REQUESTER_DASHBOARD)){
+        if (Auth::hasPermission(Permission::VIEW_REQUESTER_DASHBOARD)) {
             return 'teacher/app';
         }
         return '/entrar';
