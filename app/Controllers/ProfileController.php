288a9ce648ee5b243b83b5ddeaec0021af2083f8
Nayra Geovana de Sousa Rodrigues <nararodrygues530@gmail.com>
2026-05-14 16:41:47 -0300

    feat(controller): adicionar método update ao ProfileController

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Core\Auth;
 use App\Core\Controller;
+use App\Core\Message;
 use App\Core\Permission;
 use App\Models\User;
 
@@ -25,7 +26,8 @@ class ProfileController extends Controller
 
         echo $this->view->render('account/profile', [
             'user' => $user,
-            'layout' => $layout
+            'layout' => $layout,
+            'profile' => null
         ]);
 
         clear_old();
@@ -44,6 +46,61 @@ class ProfileController extends Controller
             return 'teacher/app';
         }
         return '/entrar';
+    }
+
+    public function update(?array $data): void
+    {
+        //$this->validateCsrfToken($data, "/perfil");
+
+        $user = User::find(Auth::user()->id);
+
+        if (!$user) {
+            Message::error("Usúario não encontrado");
+            redirect("/perfil");
+            return;
+        }
+
+        try {
+
+            $user->fill([
+                "avatar_path" => "",
+                "phone" => $data["phone"],
+                "extension" => $data["extension"],
+                "gender" => $data["gender"],
+                "birth_date" => $data["birth_date"],
+                "job_title" => $data["job_title"],
+                "registration" => $data["registration"],
+                "specialty" => $data["specialty"],
+                "bio" => $data["bio"],
+                "city" => $data["city"],
+                "country" => $data["country"],
+            ]);
+
+            $errors = array_merge(
+                $user->validate($data),
+            );
+
+            if ($errors) {
+
+                flash_old($data);
+
+                foreach ($errors as $error) {
+                    Message::warning($error);
+                }
+                redirect("/perfil");
+            }
+
+            $user->save();
+
+        } catch (\InvalidArgumentException $invalidArgumentException) {
+            Message::error($invalidArgumentException->getMessage());
+            redirect("/perfil");
+            return;
+        }
+
+        Message::success("Perfil atualizado com sucesso!");
+        redirect("/perfil");
+
     }
 
 }
