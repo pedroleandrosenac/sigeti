28b384e9ec2558762d50c1c4726bb6fee30d7453
Ezequiel Keslley Viana Santos da Cruz <ezequielcx14@gmail.com>
2026-05-14 15:53:22 -0300

    feat(controller): adicionar método store ao Admin\\DepartmentController

--- app/Controllers/Admin/DepartmentController.php
+++ app/Controllers/Admin/DepartmentController.php
@@ -20,9 +20,9 @@ class DepartmentController extends Controller
     public function index(): void
     {
         Auth::requirePermission(Permission::VIEW_DEPARTMENTS);
-        
+
         $departmentModel = new Department();
-        
+
         $departments = $departmentModel
             ->orderBy("name", "ASC")
             ->orderBy("deleted_at", "DESC")
@@ -38,14 +38,52 @@ class DepartmentController extends Controller
     public function create(): void
     {
         Auth::requirePermission(Permission::CREATE_DEPARTMENT);
-        
+
         echo $this->view->render("admin/department/create");
-        
+
         clear_old();
     }
 
-    public function ()
+    public function store(?array $data): void
     {
-        
+        Auth::requirePermission(Permission::CREATE_DEPARTMENT);
+
+        $this->validateCsrfToken($data, "/admin/departamentos/cadastrar");
+
+        $department = new Department();
+
+        try {
+            $department->fill([
+                "name" => $data["name"],
+                "code" => $data["code"],
+                "description" => $data["description"],
+                "address" => $data["address"],
+            ]);
+
+            $errors = array_merge(
+                $department->validate($data),
+                $department->validateBusinessRule()
+            );
+
+            if ($errors){
+                flash_old($data);
+
+                foreach ($errors as $error){
+                    Message::warning($error);
+                }
+                redirect("/admin/departamentos/cadastrar");
+                return;
+            }
+
+            $department->save();
+        }catch (\InvalidArgumentException $invalidArgumentException){
+            Message::error($invalidArgumentException->getMessage());
+            redirect("/admin/departamentos/cadastrar");
+            return;
+        }
+
+        Message::success("Departamento cadastrado com sucesso!");
+        redirect("/admin/departamentos/editar");
     }
+
 }
